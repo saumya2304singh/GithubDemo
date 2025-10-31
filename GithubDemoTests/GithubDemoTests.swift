118b88deeb0757495ac143fc002318e47da401f7
Saumya Singh <saumyaguru2304@gmail.com>
2025-10-31 08:24:35 +0530

    Initial Commit

---
+++ GithubDemoTests/GithubDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemoTests.swift
+//  GithubDemoTests
+//
+//  Created by user@8 on 31/10/25.
+//
+
+import Testing
+@testable import GithubDemo
+
+struct GithubDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
